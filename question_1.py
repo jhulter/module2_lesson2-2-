6edb33fe2339be7fa30a911b01873bfb396ea7b5
jhulter <79738742+jhulter@users.noreply.github.com>
2024-06-17 18:56:33 -0700

    Update question_1.py

--- question_1.py
+++ question_1.py
@@ -11,4 +11,21 @@ if place == "forest":
 elif place == "cave":
     print("You find a hidden treasure!")
 
+else:
+    pass
+
+# Task 2
+
+if place == "cave":
+    light = input("Do you want a torch or proceed in the dark? (torch/dark)")
+    if light == "torch":
+        print("You can see drawings left by neanderthals")
+    elif light == "dark":
+        print("After a few steps with no light you just fell down a huge hole and broke both your legs and will probably die now...")
+
+# Task 3
+    else:
+        pass
+else:
+    pass
 
